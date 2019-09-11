6c4ab1ba91e83cf573acb3859739257f9ffc5922
Ivan <ivan@objectbox.io>
2019-09-11 09:53:37 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -eu
 
 project=$1
 
-options=CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED="NO" CODE_SIGN_ENTITLEMENTS="" CODE_SIGNING_ALLOWED="NO"
+options="CODE_SIGN_IDENTITY=\"\" CODE_SIGNING_REQUIRED=\"NO\" CODE_SIGN_ENTITLEMENTS=\"\" CODE_SIGNING_ALLOWED=\"NO\""
 
 cd ${project}
 xcodebuild clean build ${options} -project ${project}.xcodeproj -scheme ${project} 
