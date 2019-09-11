c662141b32137bb4eae20e6b9404c33d4307afdb
Ivan <ivan@objectbox.io>
2019-09-11 10:08:04 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,22 @@ project=$1
 options="CODE_SIGN_IDENTITY=\"\" CODE_SIGNING_REQUIRED=\"NO\" CODE_SIGN_ENTITLEMENTS=\"\" CODE_SIGNING_ALLOWED=\"NO\""
 
 cd ${project}
+
+# https://swift.objectbox.io/install
+cat > Podfile << EOL
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target '${project}' do
+  use_frameworks!
+
+  # Pods for ${project}
+  pod 'ObjectBox'
+end
+EOL
+
+pod repo update
+pod install
+Pods/ObjectBox/setup.rb
+
 xcodebuild clean build ${options} -project ${project}.xcodeproj -scheme ${project} 
