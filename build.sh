ad4054cfed06f0926178eba6853ee9d0bb0d87a2
Ivan <ivan@objectbox.io>
2019-09-11 09:47:34 +0000

    disable code signing

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -eu
 project=$1
 
 cd ${project}
-xcodebuild clean build -project ${project}.xcodeproj -scheme ${project}
+xcodebuild clean build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -project ${project}.xcodeproj -scheme ${project} 
