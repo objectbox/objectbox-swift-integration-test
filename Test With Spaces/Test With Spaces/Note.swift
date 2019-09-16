e2641e54321984b81a6b0aae7f3ca2174dcdea7b
Graybox CI <graybox-ci@objectbox.io>
2019-09-16 14:06:05 +0200

    test with spaces

---
+++ Test With Spaces/Test With Spaces/Note.swift
@@ -0,0 +1,39 @@
+//
+//  Note.swift
+//  Test With Spaces
+//
+//  Created by objectbox on 16.09.19.
+//  Copyright © 2019 objectbox. All rights reserved.
+//
+
+import Foundation
+import ObjectBox
+
+class Note: Entity {
+    var id: Id = 0
+    var title: String = "" {
+        didSet {
+            modificationDate = Date()
+        }
+    }
+    var text: String = "" {
+        didSet {
+            modificationDate = Date()
+        }
+    }
+    var creationDate: Date? = Date()
+    var modificationDate: Date?
+    var author: ToOne<Author> = nil
+    
+    // An initializer with no parameters is required by ObjectBox
+    required init() {
+        // Nothing to do since we initialize the properties upon declaration here.
+        // See `Author` for a different approach
+    }
+    
+    convenience init(title: String, text: String) {
+        self.init()
+        self.title = title
+        self.text = text
+    }
+}
---
+++ Test With Spaces/Test With Spaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test With Spaces
+//
+//  Created by objectbox on 16.09.19.
+//  Copyright © 2019 objectbox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
