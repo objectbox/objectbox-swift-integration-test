049d4be5eb772c9f6941a498cdc334c28e2eb9d9
Ivan <ivan@objectbox.io>
2019-09-16 12:39:53 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ echo "========================================================================="
 
 options="CODE_SIGN_IDENTITY= CODE_SIGNING_REQUIRED=NO CODE_SIGN_ENTITLEMENTS= CODE_SIGNING_ALLOWED=NO ENABLE_BITCODE=NO"
 options="${options} -derivedDataPath ./DerivedData"
-options="${options} -workspace \"${project}.xcworkspace\" -scheme \"${project}\""
+options="${options} -workspace ${project}.xcworkspace -scheme ${project}"
 
 cd "${project}"
 
