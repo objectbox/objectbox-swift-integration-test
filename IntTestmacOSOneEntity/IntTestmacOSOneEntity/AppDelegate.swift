83ad59e21c03f000d0283d82c6abe98cf7656464
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2021-06-01 13:53:15 +0200

    Change macOS test app to an sandboxed application

---
+++ IntTestmacOSOneEntity/IntTestmacOSOneEntity/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  IntTestmacOSOneEntityApp
+//
+//  Created by Marcin Krzyzanowski on 01/06/2021.
+//  Copyright © 2021 ObjectBox. All rights reserved.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ IntTestmacOSOneEntity/IntTestmacOSOneEntity/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IntTestmacOSOneEntityApp
+//
+//  Created by Marcin Krzyzanowski on 01/06/2021.
+//  Copyright © 2021 ObjectBox. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
