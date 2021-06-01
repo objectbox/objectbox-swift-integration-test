4a79394246d7b278782a12999e02a2c026dbef0e
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2021-06-01 14:00:01 +0200

    Create store

--- IntTestmacOSOneEntity/IntTestmacOSOneEntity/AppDelegate.swift
+++ IntTestmacOSOneEntity/IntTestmacOSOneEntity/AppDelegate.swift
@@ -7,15 +7,27 @@
 //
 
 import Cocoa
+import ObjectBox
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
-
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+
+        let databaseName = "test"
+        let appSupport = try! FileManager.default.url(for: .applicationSupportDirectory,
+                                                     in: .userDomainMask,
+                                                     appropriateFor: nil,
+                                                     create: true).appendingPathComponent(Bundle.main.bundleIdentifier!)
+
+        let directory = appSupport.appendingPathComponent(databaseName)
+        try? FileManager.default.createDirectory(at: directory,
+                                                 withIntermediateDirectories: true,
+                                                 attributes: nil)
+
+        let store = try! Store(directoryPath: directory.path)
+
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
